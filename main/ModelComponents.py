13049d24623f8149e6f6b81078173160cd08e1c8
Ajay <jaykumar002kori@gmail.com>
2024-07-07 15:02:06 +0530

    commit

--- main/ModelComponents.py
+++ main/ModelComponents.py
@@ -1,5 +1,5 @@
 from sklearn.model_selection import train_test_split
-#from utils.logger import logging
+from utils.logger import logging
 import pandas as pd 
 import os
 
@@ -14,25 +14,31 @@ class DataIntake:
         directory='artifacts/Data'
         os.makedirs(directory, exist_ok=True)
         
+        logging.info('Checking the file type in DataIntake Class.')
         if row_data_path.endswith('.csv'):
             df=pd.read_csv(row_data_path)
+            logging.info('Reading the file as Pandas dataframe.')
             X=df.iloc[:,:-1]
             y=df['seedType']
 
             # Split the data into training and test sets
+            logging.info('Split the data into training and test sets.')
             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
             test_df=pd.concat([X_test, y_test], axis=1)
             train_df=pd.concat([X_train, y_train], axis=1)
             
+            logging.info('Saving the data into training and test sets.')
             TestDataFilePath=os.path.join(directory, 'TestData.csv')
             TrainDataFilePath=os.path.join(directory, 'TrainData.csv')
             
+            logging.info('Converting the train df and test df into csv file.')
             test_df.to_csv(TestDataFilePath, sep=',', index=False)
             train_df.to_csv(TrainDataFilePath, sep=',', index=False)
             
+            logging.info('Returning the file path of Train and Test datasets.')
             return (TrainDataFilePath, TestDataFilePath)
         else:
-            #logging.info('File must be .csv')
+            logging.info('File must be .csv')
             return 
     
     
--- main/ModelPileline.py
+++ main/ModelPileline.py
@@ -12,32 +12,40 @@ class ModelTrainer:
     
     def __init__(self) -> None:
         pass
-    
+    logging.info('Model Trainer Class.')
     def TrainModel(self):
         
+        logging.info('Getting the data.')
         Data=DataIntake()
         row_data_file_path='artifacts\Data\seed_dataset.csv'
         train_df_filepath, test_df_filepath=Data.split_data(row_data_path=row_data_file_path)
+        logging.info('Getting the train and test data file path.')
         
+        logging.info('Reading the data into csv file in pandas dataframe.')
         train_df=pd.read_csv(train_df_filepath)
         test_df=pd.read_csv(test_df_filepath)
         
+        logging.info('Split the data into training and test sets.')
         X_train=train_df.iloc[:,:-1]
         y_train=train_df['seedType']
         
         X_test=test_df.iloc[:,:-1]
         y_test=test_df['seedType']
         
+        logging.info('Creating the SVC model.')
         SVC_Model=SVC(C=100, kernel='linear', probability=True)
         SVC_Model.fit(X_train, y_train)
         
+        logging.info('Predicting the test data.')
         y_pred=SVC_Model.predict(X_test)
         
+        logging.info('Checking various matrix for model performance.')
         accuracy=accuracy_score(y_test, y_pred)
         precision = precision_score(y_test, y_pred, average='weighted')
         recall = recall_score(y_test, y_pred, average='weighted')
         f1 = f1_score(y_test, y_pred, average='weighted')
         conf_matrix = confusion_matrix(y_test, y_pred)
+        
         
         Model_matrix={
             'Accuracy': accuracy,
@@ -49,19 +57,23 @@ class ModelTrainer:
         logging.info(f'Model Performace Details:\n {str(Model_matrix)}')
         
         ModelFilePath=save_model('SVC_model.pkl', model_obj=SVC_Model)
+        logging.info('Returning the model file path after saving the model.')
         return ModelFilePath
         
 class PredictionPipeline():
     def __init__(self) -> None:
         pass
-        
+    
+    logging.info('Prediction Pipeline Class.')
     def Prediction(self, newdata):
-        
+        logging.info('Getting the model object and loading for predicting new datapoints.')
         Trainer=ModelTrainer()
         modelfilepath=Trainer.TrainModel()
         model=load_model(modelfilepath)
         
+        logging.info('Predicting the new data points.')
         pred=model.predict(newdata)
+        logging.info(f'Model classification for new data point {pred}.')
         return pred
         
             
